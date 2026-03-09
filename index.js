2d584e3f363b6c895b2a22addbea4b7f0612ff16
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-03-09 16:19:50 +0530

    Port update in backend

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
 
-    console.log("Server is running on port 3000 !")
-
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get("/", (req, res) => {
   res.send("Task Manager Backend is running");
